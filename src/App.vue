763f0ba79f3f587e1f9b44c79439cbe7958644c6
Piotr9 <33264584+Piotr9@users.noreply.github.com>
2019-11-17 01:58:50 +0100

    Connect app to NASA API

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,11 @@
   <div id="app">
     <div class="nav">
         <router-link to="/">Spacer</router-link>
-        <router-link to="/about">Trace</router-link>
+        <router-link to="/about">About</router-link>
     </div>
 
     <router-view/>
   </div>
 </template>
 
-<style lang="scss" scoped>
+<style lang="scss" scoped />
--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,20 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import Search from '../views/Search.vue';
+
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home,
+    name: 'Search',
+    component: Search,
   },
   {
     path: '/about',
-    name: 'about',
-    // route level code-splitting
+    name: 'About',
+    // route lev el code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
